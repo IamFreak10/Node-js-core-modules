75c848afb74aa870088d714d14f5efbd9894d75f
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2026-01-09 02:45:04 +0600

    File Appended and Log data

--- append-file.js
+++ append-file.js
@@ -1,5 +1,6 @@
 const fs=require('fs');
 // fs.writeFileSync("./output/app.log","Appliciation Started\n")
 // console.log("File Write Sync");
-const logEntry1=`${new Date().toLocaleString()} user logged in\n`;
+const logEntry1=`${new Date().toISOString()} user logged in\n`;
 fs.appendFileSync("./output/app.log",logEntry1);
+const logEntry2=~`${new Date().toISOString()} user logged out\n`;
--- write-file.js
+++ write-file.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-// const content1 = 'This Is A Content \n nodejs is awsome!!';
-// try {
-//     fs.writeFileSync("./output/test-sync.txt", content1);
-//     console.log("File Write Sync");
-// } catch (error) {
-//     console.error(error.message);
-// }
+const content1 = 'This Is A Content \n nodejs is awsome!!';
+try {
+    fs.writeFileSync("./output/test-sync.txt", content1);
+    console.log("File Write Sync");
+} catch (error) {
+    console.error(error.message);
+}
 const conetnt2="Thsi Is a conent tooo \n asyncchronus!!";
 fs.writeFile("./output/test-async.txt",conetnt2,(error)=>{
     if(error){
